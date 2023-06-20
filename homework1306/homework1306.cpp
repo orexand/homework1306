57986037145590eb4374ecf43d6516179ec0810d
Ilya <Ilya@DESKTOP-HQ1UA1U>
2023-06-20 13:34:53 +0500

    Добавьте файлы проекта.

---
+++ homework1306/homework1306.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+void sortQueue(queue<int>& q) {
+    int n = q.size();
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++) {
+        v[i] = q.front();
+        q.pop();
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++) {
+        q.push(v[i]);
+    }
+}
+
+int main() {
+    queue<int> q;
+    for (int i = 0; i < 10; i++) {
+        q.push(rand() % 100);
+    }
+
+    sortQueue(q);
+
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
